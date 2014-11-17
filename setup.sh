54992ed84dd96463ae584f6068349fc643e21b01
arnski <krueger-wauschkuhn.arne@guj.de>
2014-11-17 15:01:43 +0100

    Adding .gitignore, osx-files/functions and toggle_hidden, setup.sh, command_exists

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# @ToDo: Backup & Set Symlinks
+
+# Install missing binaries
+
+# @ToDo: Check if on OSX
+
+# Check if brew installed
+
+# Installs via brew
+
+## Autojump
+brew doctor && brew install autojump
